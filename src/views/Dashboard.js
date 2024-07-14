3553e905640789a832512f0f7f603f5fac249164
cristi11191 <cristin.druta@mail.ru>
2024-07-14 21:47:14 +0300

    Structure Changes

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,14 +1,14 @@
 
 import React ,{useState} from 'react';
 import { useNavigate,Navigate } from 'react-router-dom';
-import SearchBox from './SearchBox';
-import Overview from './Overview';
-import Activity from './Activity';
+import SearchBox from '../components/SearchBox';
+import Overview from '../components/Overview';
+import Activity from '../components/Activity';
 import { UilBars } from '@iconscout/react-unicons';
 import useSidebar from '../hooks/useSidebar';
 import apiService from '../services/apiService';
 import '../styles/styles.css';
-import Navbar from './Navbar';
+import Navbar from '../components/Navbar';
 const Dashboard = () => {
   const [isSidebarOpen, toggleSidebar] = useSidebar();
   const [primaryColor, setPrimaryColor] = useState('#3498db');
